baec3d5be7cdda1b608e04f7d8ec9acd91f2a112
Daryn Holmes <daryn@Mac.local>
2015-07-30 21:19:52 +0100

    Adding

--- project_person/person_test.rb
+++ project_person/person_test.rb
@@ -6,7 +6,8 @@ class TestPerson < MiniTest::Unit::TestCase
     first_name = 'John'
     last_name = 'Snow'
     full_name = first_name + last_name
-    assert_equal('John Snow', full_name)    
+    assert_equal('John Snow', full_name)
+    hello    
   end
   
 end
