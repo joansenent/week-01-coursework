28839c1b327b2e97ad358a293e75f28846e58df2
[joansenent] <joansenent@hotmail.com>
2015-08-02 20:14:21 +0100

    person class and test successful commit

---
+++ project_person/person.rb
@@ -0,0 +1,16 @@
+class Person
+
+	def initialize(first_name, last_name)
+		@first_name = first_name
+		@last_name = last_name
+	end
+
+	def full_name()
+		@first_name + " " + @last_name
+	end
+
+	def set_first_name(new_name)
+		@first_name = new_name
+	end
+
+end
--- project_person/person_test.rb
+++ project_person/person_test.rb
@@ -1,12 +1,20 @@
 require 'minitest/autorun'
+require_relative 'person'
 
 class TestPerson < MiniTest::Unit::TestCase
     
   def test_full_name
     first_name = 'John'
     last_name = 'Snow'
-    full_name = first_name + last_name
+    person = Person.new(first_name, last_name)
+    full_name = person.full_name
     assert_equal('John Snow', full_name)
+  end
+
+  def test_set_first_name
+  	person = Person.new('John', 'Snow')
+  	person.set_first_name('Mark')
+  	assert_equal('Mark Snow', person.full_name) 
   end
   
 end
